8aaf44cd1fdf7b85fa9f1e2393636a7792935195
rick2785 <rjhrabowskie@gmail.com>
2020-09-29 07:42:27 -0500

    Initial Commit

---
+++ WheelRadialPicker/WheelRadialPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelRadialPickerApp.swift
+//  WheelRadialPicker
+//
+//  Created by RJ Hrabowskie on 9/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WheelRadialPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
